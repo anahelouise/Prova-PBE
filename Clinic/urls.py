94ecb320755cb618b7de8b44a1f5c31c62c0d08a
Ana Helouise <anahelouise.ss@gmail.com>
2024-09-19 22:18:56 -0300

    prove_anahelouise

---
+++ Clinic/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for Clinic project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.http import HttpResponse
+from django.contrib import admin
+from django.urls import path
+
+
+def home(request):
+     return HttpResponse("Bem-vindos a clinica Aumigos")
+
+def perfil_usuario(request, username):
+     return HttpResponse(f"Bem-vindos ao perfil dos Veterinarios: {username}")
+
+def contato(request):
+     return HttpResponse("Entre em contato com a gente pelo nosso telefone (35)98888-7777 ou pelo nosso email: contato_aumigos@gmail.com")
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home),
+    path('veterinario/<str:username>/', perfil_usuario),  
+    path('contato/', contato), 
+]
